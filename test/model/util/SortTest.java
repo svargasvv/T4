a371df73dac89d09b764cf26a4eef414f7e197f6
Fernando De la Rosa <fde@uniandes.edu.co>
2019-02-14 22:33:22 -0500

    correccion clase de prueba Sort

--- test/model/util/SortTest.java
+++ test/model/util/SortTest.java
@@ -1,19 +1,25 @@
-package model.logic;
+package model.util;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import model.util.Sort;
+
 public class SortTest {
 
+	// Muestra de datos a ordenar
+	private Comparable[] datos;
+	
 	@Before
 	public void setUp() throws Exception{
-		System.out.println("Codigo de iniciacion");
+		System.out.println("Codigo de configuracion de muestra de datos a probar");
 	}
 
 	@Test
 	public void test() {
+		Sort.ordenarMergeSort(datos);
 		fail("Not yet implemented");
 	}
 
